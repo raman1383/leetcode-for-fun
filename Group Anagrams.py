dc51c8d02ca91ef8a01a60518050305a9150dfa1
raman1383 <raman83shariati@gmail.com>
2022-08-31 11:51:34 +0430

    r

---
+++ Group Anagrams.py
@@ -0,0 +1,11 @@
+import collections
+
+
+def groupAnagram(self, strs: list[str]) -> list[list[int]]:
+    ans = collections.defaultdict(list)
+    for s in strs:
+        count = [0] * 26
+        for c in s:
+            count[ord(c) - ord('a')] += 1
+        ans[tuple(count)].append(s)
+    return ans.values()
